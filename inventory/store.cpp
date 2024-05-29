9750fb8e1511a1b1b498e7dd8510ae5a002e334a
blessingmad <mad22008@byui.edu>
2024-05-29 05:27:08 +0200

    program running now

--- inventory/store.cpp
+++ inventory/store.cpp
@@ -4,6 +4,10 @@ using namespace std;
 int main() {
   cout << "This Program Manages Inventory System with Items, Quantities, and Prices.";
   cout << "Items";
+
+  cout << endl;
+
+  cout << "Test ";
   
   return 0;
 }
